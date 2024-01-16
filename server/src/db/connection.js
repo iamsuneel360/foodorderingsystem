00c19f93c833b7e68b2c08dda3b2b995aef985e7
i am Sunil <bistasunil360@gmail.com>
2024-01-16 14:02:19 +0545

    setup

---
+++ server/src/db/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connection = async () => {
+  try {
+    const isConnected = await mongoose.connect(
+      "mongodb://127.0.0.1:27017/hotelmgmtDB"
+    );
+    if (isConnected) {
+      console.log("Connected to mongodb");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = connection;
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+const connection = require("./db/connection");
 require("dotenv").config();
 const port = process.env.PORT;
 const userRoute = require("./routes/user");
 app.use(userRoute);
+connection();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
