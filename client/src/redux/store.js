6c8dc0b057907f8ec230cba19e5f42ccc216dba2
i am Sunil <bistasunil360@gmail.com>
2024-02-09 17:03:29 +0545

    fixed error

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import countSlice from "./reducerSlice/countSlice";
-import userReducer from "./reducerSlice/userSlice"
+import userReducer from "./reducerSlice/userSlice";
 import logger from "redux-logger";
-
 
 export default configureStore({
   reducer: {
-    count: countSlice,
-    user: userReducer
+    user: userReducer,
   },
-  middleware: ()=>[logger]
+  middleware: () => [logger],
 });
